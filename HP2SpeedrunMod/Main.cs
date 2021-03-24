4eb999d7b1a31defce5725c439f105ac859c3fc6
Buurazu <excel_saga_rules@hotmail.com>
2021-03-24 03:09:10 -0400

    version number change

--- HP2SpeedrunMod/Main.cs
+++ HP2SpeedrunMod/Main.cs
@@ -24,7 +24,7 @@ namespace HP2SpeedrunMod
         /// <summary>
         /// The version of this plugin.
         /// </summary>
-        public const string PluginVersion = "1.8";
+        public const string PluginVersion = "1.8.1";
 
         //no item list yet
         //public static Dictionary<string, int> ItemNameList = new Dictionary<string, int>();
